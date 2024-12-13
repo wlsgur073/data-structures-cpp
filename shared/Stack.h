265f46c94350ff792e66f6570cc5a49122e0a896
wlsgur073 <wlsgur073@gmail.com>
2024-12-13 15:12:54 +0900

    stack commit

---
+++ shared/Stack.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+
+#include <cassert>
+#include <iostream>
+
+template<typename T> // 템플릿 사용
+class Stack
+{
+public:
+	Stack(int capacity = 1)
+	{
+		assert(capacity > 0);
+		Resize(capacity);
+	}
+
+	~Stack()
+	{
+		if (stack_) delete[] stack_;
+	}
+
+	void Resize(int new_capacity)
+	{
+		T* new_stack = new T[new_capacity];
+		memcpy(new_stack, stack_, sizeof(T) * Size());
+		if (stack_) delete[] stack_;
+		stack_ = new_stack;
+		capacity_ = new_capacity;
+	}
+
+	bool IsEmpty() const
+	{
+		return (top_ == -1);
+	}
+
+	int Size() const
+	{
+		return top_ + 1;
+	}
+
+	void Print()
+	{
+		using namespace std;
+
+		for (int i = 0; i < Size(); i++) // Size() 사용
+			cout << stack_[i] << " ";
+		cout << endl;
+	}
+
+	// Returns TOP element of stack.
+	T& Top() const
+	{
+		// assert(!IsEmpty());
+		if (IsEmpty()) return stack_[0];
+
+		return stack_[top_];
+	}
+
+	// Insert item into the TOP of the stack
+	void Push(const T& item)
+	{
+		if (top_ == capacity_ - 1)
+			Resize(capacity_ * 2);
+		
+		this->stack_[++top_] = item;
+	}
+
+	// Delete the TOP element of the stack
+	void Pop()
+	{
+		assert(!IsEmpty());
+
+		this->stack_[top_--].~T(); // optional
+	}
+
+protected: // 뒤에서 상속해서 사용
+	T* stack_ = nullptr;
+	int top_ = -1; // 인덱스, 아무것도 없을 때는 -1.
+	int capacity_ = 0;
+};
