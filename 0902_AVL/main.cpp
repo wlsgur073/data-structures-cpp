cbec091f504cae0c3c670c65e7b677eb4fcc7181
wlsgur073 <wlsgur073@gmail.com>
2024-12-24 22:06:19 +0900

    `Insert`할때 재귀적으로 호출하기에 스택에 쌓인 순서대로 하나씩 꺼내오고 그 node를 기준으로 rotation이 진행된다. 그것만 주의하면 rotation 자체는 어렵지 않다.

---
+++ 0902_AVL/main.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+#include "../shared/AVL.h"
+
+using namespace std;
+
+int main()
+{
+	using Item = AVL<int, char>::Item;
+
+	AVL<int, char> bst;
+
+	for (int i : { 9, 8, 7, 6, 5, 4, 3, 2, 1 })
+		//for (int i : { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+	{
+		bst.Insert(Item{ i, char('A' + i) });
+		bst.Print2D();
+	}
+
+	// bst.Inorder(); // key에 대해 정렬
+
+	// cout << bst.RecurGet(5)->value << endl;
+	// cout << bst.RecurGet(3)->value << endl;
+	// cout << bst.RecurGet(7)->value << endl;
+	// cout << bst.RecurGet(1)->value << endl;
+	// cout << bst.RecurGet(4)->value << endl;
+
+	cout << "------------------------------------" << endl;
+	for (int i : {5, 8, 9})
+	{
+		bst.Remove(i);
+		bst.Print2D();
+	}
+
+	return 0;
+}
+
+/*
+	트리의 왼쪽, 오른쪽의 균형이 맞지 않으면 좌우 트리의 높이를 비교한다.
+
+	밸런스 = 왼쪽 서브트리의 높이 - 오른쪽 서브트리의 높이
+	밸런스가 -1 이상 1이하이면 좋은 것으로 판단
+
+	가시화 도구: https://www.cs.usfca.edu/~galles/visualization/AVLtree.html
+
+	1. LL -> Right Rotation
+	왼쪽으로 노드가 치우쳐 있는 경우, 어떤 노드의 밸런스가 1보다 크고 왼쪽 자신 노드의 밸런스가 0이상.
+	2. RR -> Left Rotation
+	오른쪽으로 노드가 치우쳐 있는 경우, 어떤 노드의 밸런스가 -1보다 작고 오른쪽 자신 노드의 밸런스가 0이하.
+	3. LR -> Left-Right Rotation
+	어떤 노드의 밸런스가 1보다 크고, 왼쪽 자식 노드의 밸런스가 0보다 작을 경우.
+	왼쪽 자식 노드에 대해 RotateLeft() 후에 RotateRight().
+	4. RL -> Right-Left Rotation
+	어떤 노드의 밸런스가 -1보다 작고, 오른쪽 자식 노드의 밸런스가 0보다 클 경우.
+	오른쪽 자식 노드에 대해 RotateRight() 후에 RotateLeft().
+
+	https://www.geeksforgeeks.org/introduction-to-avl-tree/
+*/
---
+++ shared/AVL.h
@@ -0,0 +1,186 @@
+﻿#pragma once
+
+#include "BinarySearchTree.h"
+
+template<typename K, typename V>
+class AVL : public BinarySearchTree<K, V>
+{
+public:
+	using Base = BinarySearchTree<K, V>;
+	using typename BinarySearchTree<K, V>::Item;
+    using typename BinarySearchTree<K, V>::Node;
+
+	//struct Item {
+	//	K key = K();	// first
+	//	V value = V();	// second
+	//};
+
+	//struct Node {
+	//	Item item;
+	//	Node* left = nullptr;
+	//	Node* right = nullptr;
+	//};
+
+	int Height(Node* node)
+	{
+		return Base::Height(node);
+	}
+
+	int Balance(Node* node)
+	{
+		if (node)
+			return Base::Height(node->left) - Base::Height(node->right); // 왼쪽이 클 경우 양수, 오른쪽이 클 경우 음수
+		else
+			return 0;
+	}
+
+	Node* RotateLeft(Node* parent)
+	{
+		// 만약 A-B-C 중, B의 자식 노드가 2개 다 있을 경우, B의 왼쪽 노드를 A의 자식 노드로 처리해줘야 한다.
+		Node* child = parent->right;
+		parent->right = child->left;
+		child->left = parent;
+		return child;
+	}
+
+	Node* RotateRight(Node* parent)
+	{
+		Node* child = parent->left;
+		parent->left = child->right;
+		child->right = parent;
+		return child;
+	}
+
+	void Insert(const Item& item)
+	{
+		root_ = Insert(root_, item);
+	}
+
+	Node* Insert(Node* node, const Item& item)
+	{
+		if (!node)
+			return new Node{ item, nullptr, nullptr };
+
+		const auto& key = item.key;
+
+		if (key < node->item.key)
+			node->left = Insert(node->left, item);
+		else if (key > node->item.key)
+			node->right = Insert(node->right, item);
+		else
+		{
+			node->item = item;
+			return node;
+		}
+
+		int balance = Balance(node);
+
+		// balance가 0, 1, -1 이면 조절할 필요가 없다고 판단
+		// 밸런스는 위에 재귀 함수인 insert 스택 순서대로 수행된다.
+		// LL -> Right Rotation
+		if (balance > 1 && Balance(node->left) >= 0)
+			return RotateRight(node);
+		
+
+		// RR -> Left Rotation
+		if (balance < -1 && Balance(node->right) <= 0)
+			return RotateLeft(node);
+
+		// LR -> Left-Right Rotation
+		if (balance > 1 && Balance(node->left) < 0)
+		{
+			node->left = RotateLeft(node->left);
+			return RotateRight(node);
+		}
+
+		// RL -> Right-Left Rotation
+		if (balance < -1 && Balance(node->right) > 0)
+		{
+			node->right = RotateRight(node->right);
+			return RotateLeft(node);
+		}
+
+		return node;
+	}
+
+	Node* MinKeyNode(Node* node)
+	{
+		assert(node);
+		while (node->left)
+			node = node->left;
+		return node;
+	}
+
+	void Remove(const K& key)
+	{
+		using namespace std;
+		cout << "Remove " << key << endl;
+		root_ = Remove(root_, key);
+	}
+
+	Node* Remove(Node* node, const K& key)
+	{
+		// BST와 동일
+		if (!node) return node;
+
+		if (key < node->item.key)
+			node->left = Remove(node->left, key);
+		else if (key > node->item.key)
+			node->right = Remove(node->right, key);
+		else
+		{
+			if (!node->left)
+			{
+				Node* temp = node->right;
+				delete node;
+				return temp;
+			}
+			else if (!node->right)
+			{
+				Node* temp = node->left;
+				delete node;
+				return temp;
+			}
+
+			Node* temp = MinKeyNode(node->right);
+			node->item = temp->item;
+			node->right = Remove(node->right, temp->item.key);
+		}
+
+		//if (node == NULL)	return node; // 불필요 (입력 node의 자식이 하나이거나 없는 경우에는 위에서 return 하기 때문)
+
+		// 균형 잡기
+
+		int balance = Balance(node);
+
+		if (node == NULL) return node;
+		
+		if (balance > 1 && Balance(node->left) >= 0)
+			return RotateRight(node);
+		
+
+		// RR -> Left Rotation
+		if (balance < -1 && Balance(node->right) <= 0)
+			return RotateLeft(node);
+
+		// LR -> Left-Right Rotation
+		if (balance > 1 && Balance(node->left) < 0)
+		{
+			node->left = RotateLeft(node->left);
+			return RotateRight(node);
+		}
+
+		// RL -> Right-Left Rotation
+		if (balance < -1 && Balance(node->right) > 0)
+		{
+			node->right = RotateRight(node->right);
+			return RotateLeft(node);
+		}
+
+		return node;
+	}
+
+private:
+	Node*& root_ = BinarySearchTree<K, V>::root_;
+	// this->root_로도 사용 가능
+};
