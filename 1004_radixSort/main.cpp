a5afe731e3d8a53b412300541b5199fdbf2d3b0d
wlsgur073 <wlsgur073@gmail.com>
2025-01-13 12:39:44 +0900

    - 시간 복잡도: 정수가 n 개이고 최대 자리수가 d 자리라면 (예: 802는 3자리, d = 3) 시간 복잡도는 O(d n)로 계산할 수 있다. 컴퓨터에서 사용하는 정수의 자리수에는 제한이 있기 때문에 최대 d를 상수로 추리하면 O(n)으로 볼 수도 있다. 대신에, 실수나 문자열 등을 정렬하기는 쉽지 않다.
    - 공간 복잡도: 일단 n 개의 값들을 모두 옮겨담을 추가 메모리가 필요. 추가로 w 진수라면 (10진수라면 10개) 큐 w개를 다루기 위해 필요한 추가 메모리 공간이 필요. 그래서 O(n + w)로 분석할 수 있다. 큐를 사용하는 대신에 `counting sort`를 사용하는 방식도 있는데 그때는 분석이 조금 달라지지만 어쨌든 최소 n의 추가 메모리는 필요. 그래서 메모리를 아껴야하는 상황에서는 `quick sort` 같이 추가 메모리가 필요 없는 방식을 선호. 반대로 실시간 게임이나 GPU 연산에서는 radix sort도 많이 사용.
    
    - bitonic sort refer: [link](https://developer.nvidia.com/gpugems/gpugems3/part-vi-gpu-computing/chapter-39-parallel-prefix-sum-scan-cuda)

---
+++ 1004_radixSort/main.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include "../shared/Queue.h"
+
+using namespace std;
+
+bool CheckSorted(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+void Print(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int GetMax(int arr[], int n)
+{
+	int mx = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] > mx)
+			mx = arr[i];
+	return mx;
+}
+
+int main()
+{
+	int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	Print(arr, n);
+
+	Queue<int> queues[10]; // 미리 10개의 큐를 만들어 놓는다. // FIFO이기에 원래 있던 숫자에 변화가 없어, stable sort
+
+	int m = GetMax(arr, n); // 가장 큰 자리수를 찾기 위해서
+
+	cout << "Max = " << m << endl;
+	cout << "========================" << endl;
+
+	for (int exp = 1; m / exp > 0; exp *= 10) 
+	{
+		for (int i = 0; i < n; i++)
+			queues[(arr[i] / exp) % 10].Enqueue(arr[i]); // 자리수 기준으로 큐 분류.
+		
+		int idx = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			while (!queues[i].IsEmpty())
+			{
+				arr[idx] = queues[i].Front();
+				queues[i].Dequeue();
+				idx++;
+			}
+		}
+
+		Print(arr, n);
+	}
+
+	return 0;
+}
+
+
+/*
+1의 자리에 대해 정렬
+10의 자리에 대해 정렬 -> 2같은 경우 앞에 0을 붙여 02로 만들어 주고 비교함.
+100의 자리에 대해 정렬
+*/
