1ffc10cc9f6ba20525c03fa7ecc21717b6f0c578
wlsgur073 <wlsgur073@gmail.com>
2025-01-15 12:05:46 +0900

    원래 배열의 크기가 n이고 인덱스 테이블의 크기가 m 이라고 하면 O(m + n / m)으로 분석할 수 있음. 예를 들어, n이 1000이고 m이 10이라고 하면 선형 탐색으로는 1000개를 하나하나 다 찾아봐야 하는데, 색인 순차 탐색에서는 10개 표 찾아보기 + 10개의 선형탐색이 되기 때문에 훨씬 효율이 높음. 만약 선형탐색 대신 이진 탐색을 하게 되면 O(m + log(n/m))이므로 더 괜찮을 듯.

---
+++ 1102_indexedSequentialSearch/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int SequentialSearch(int arr[], int start, int stop, int x)
+{
+	for (int i = start; i < stop; i++)
+	{
+		if (arr[i] == x)
+			return i;
+	}
+
+	return -1;
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 8, 13, 22, 45, 55, 75, 88 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	int keys[] = { 1, 13, 55 };
+	int kn = sizeof(keys) / sizeof(keys[0]);
+
+	int indices[] = { 0, 3, 6, n }; // 마지막에 n 하나 더 추가
+
+	for (int x : {1, 2, 8, 13, 22, 45, 55, 75, 88, 99, -1, 47, 101, 1000, 7, 10, 50, 60 })
+	{
+		if (x < arr[0] || x > arr[n - 1])
+			cout << x << " was not found" << endl;
+		else
+		{
+			// while (x > arr[indices[i]]) // 단순히 배열 테이블을 순차적으로 검색
+
+			// 인덱스 테이블에서 검색할 범위 결정
+            int k = 0;
+            while (k < kn - 1 && keys[k + 1] <= x)
+                k++;
+
+            // 색인 표의 범위에 따라서 선형 탐색
+            // indices[k] 이상 indices[k+1] 미만에 대해 검색
+            // 이때 indices 배열 마지막에 추가로 n을 저장해뒀기 때문에 k+1 사용 가능
+            int found = SequentialSearch(arr, indices[k], indices[k + 1], x);
+            if (found >= 0)
+                cout << x << " was found at " << found << endl;
+            else
+                cout << x << " was not found" << endl;
+		}
+	}
+
+	return 0;
+}
+
+/*
+	색인(index) 순차 탐색
+*/
