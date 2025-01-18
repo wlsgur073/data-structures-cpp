a2ac8ef1bc25780437c972a512662a4793990435
wlsgur073 <wlsgur073@gmail.com>
2025-01-18 15:10:23 +0900

    **다시 수정** 아 근데, 위 방식으로 하면 첫 for 반복문 `i = (i + 1) % capacity_`에서 i가 쓰레기 값이 들어갈 수 있다. i가 실수인 경우 굉장히 위험한 반복문이 될 수 있으므로, Horowitz 교제에 나온 방식으로 다시 수정한다. Horowitz 교제 방식을 따르면, Front가 맨 마지막으로 resize가 되는데, 보기 불편해도, 좀 더 stable하다.
    출력문
    ```
    0 1
    Cap = 2, Size = 1
     R  F
     0  1
     1  *
    
    -1163005939 2
    Cap = 4, Size = 2
     F     R
     0  1  2  3
     - -1163005939  2  *
     ```

--- shared/Queue.h
+++ shared/Queue.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <iomanip>
 
-template<typename T>
+template <typename T>
 class Queue // Circular Queue
 {
 public:
@@ -19,7 +19,8 @@ public:
 
 	~Queue()
 	{
-		if (queue_) delete[] queue_;
+		if (queue_)
+			delete[] queue_;
 	}
 
 	bool IsEmpty() const
@@ -33,14 +34,14 @@ public:
 		return (rear_ + 1) % capacity_ == front_;
 	}
 
-	T& Front() const
+	T &Front() const
 	{
 		assert(!IsEmpty());
 
 		return queue_[(front_ + 1) % capacity_]; // 주의 + 1
 	}
 
-	T& Rear() const
+	T &Rear() const
 	{
 		assert(!IsEmpty());
 
@@ -56,42 +57,58 @@ public:
 			return rear_ - front_;
 	}
 
-	void Resize() 
+	void Resize()
 	{
 		// 이미 IsFull()에서 front_ == rear_가 성립이 안된다는 가정하에.
 		// 경우를 나눠서 memcpy()로 블럭 단위로 복사하면 더 효율적.
 
-		T* new_queue = new T[capacity_ * 2];
+		T *new_queue = new T[capacity_ * 2];
 
 		// if (front_ < rear_) // front가 rear보다 앞에 있을 때
 		// {
 		// 	memcpy(new_queue, queue_ + front_, sizeof(T) * (rear_ - front_ + 1)); // front 다음부터 rear까지
 		// }
-		// else // rear < front 일 때 두 번에 나눠서 복사 
+		// else // rear < front 일 때 두 번에 나눠서 복사
 		// {
 		// 	int front_to_end = capacity_ - front_ - 1;
 		// 	int first_to_rear = rear_ + 1;
-			
+
 		// 	memcpy(new_queue + front_to_end, queue_, sizeof(T) * first_to_rear); // 처음부터 rear까지
 		// 	memcpy(new_queue, queue_ + front_ + 1, sizeof(T) * front_to_end); // front 다음부터 끝까지
 		// }
 
-		int count = 1; // 앞에 하나 띄어놔야 함
-		for (int i = (front_ + 1) % capacity_; i != (rear_ + 1) % capacity_; i = (i + 1) % capacity_)
-			new_queue[count++] = queue_[i];
-		
-		front_ = 0;
-		rear_ = capacity_ - 1; // resize했다는건 현재 rear가 capacity - 1이라는 뜻
+		// int count = 1; // 앞에 하나 띄어놔야 함
+		// for (int i = (front_ + 1) % capacity_; i != (rear_ + 1) % capacity_; i = (i + 1) % capacity_)
+		// 	new_queue[count++] = queue_[i];
+
+		// front_ = 0;
+		// rear_ = capacity_ - 1; // resize했다는건 현재 rear가 capacity - 1이라는 뜻
+		// capacity_ *= 2;
+		// delete[] queue_;
+		// queue_ = new_queue;
+
+		int start = (front_ + 1) % capacity_;
+		if (start < 2) // front < rear 라서 한 번에 복사
+			memcpy(new_queue, queue_ + start, sizeof(T) * (capacity_ - 1));
+		else // rear < front 라서 두 번에 나눠서 복사 (배열의 가운데 부분이 비어있는 상태)
+		{
+			// 원래 queue_의 오른쪽 끝부분(front + 1 에서부터 끝까지)를 new_queue의 앞에 복사
+			memcpy(new_queue, queue_ + start, sizeof(T) * (capacity_ - start));
+			// 원래 quene_의 왼쪽 처음부터 rear까지를 new_queue에 이어서 복사
+			memcpy(new_queue + capacity_ - start, queue_, sizeof(T) * (rear_ + 1));
+		}
+		front_ = 2 * capacity_ - 1; // front를 0으로 옮김
+		rear_= capacity_ - 2; // rear를 capacity - 1로 옮김
 		capacity_ *= 2;
 		delete[] queue_;
 		queue_ = new_queue;
 	}
 
-	void Enqueue(const T& item) // 맨 뒤에 추가, Push()
+	void Enqueue(const T &item) // 맨 뒤에 추가, Push()
 	{
 		if (IsFull())
 			Resize();
-		
+
 		rear_ = (rear_ + 1) % capacity_; // 꽉 차면 다시 [0]으로 돌아가야 함
 		queue_[rear_] = item;
 	}
@@ -108,9 +125,9 @@ public:
 		using namespace std;
 
 		// front 다음부터 rear까지 출력
-		 for (int i = (front_ + 1) % capacity_; i != (rear_ + 1) % capacity_; i = (i + 1) % capacity_)
-		 	cout << queue_[i] << " ";
-		 cout << endl;
+		for (int i = (front_ + 1) % capacity_; i != (rear_ + 1) % capacity_; i = (i + 1) % capacity_)
+			cout << queue_[i] << " ";
+		cout << endl;
 
 		if (print_debug_)
 			PrintDebug();
@@ -124,10 +141,14 @@ public:
 		cout << endl;
 
 		// front와 rear 위치 표시
-		for (int i = 0; i < capacity_; i++) {
-			if (i == front_) cout << " F ";
-			else if (i == rear_) cout << " R ";
-			else cout << "   ";
+		for (int i = 0; i < capacity_; i++)
+		{
+			if (i == front_)
+				cout << " F ";
+			else if (i == rear_)
+				cout << " R ";
+			else
+				cout << "   ";
 		}
 		cout << endl;
 
@@ -150,7 +171,8 @@ public:
 			for (int i = rear_ + 1; i < capacity_; i++)
 				cout << " * ";
 
-			cout << endl << endl;
+			cout << endl
+				 << endl;
 		}
 		else if (front_ > rear_)
 		{
@@ -166,13 +188,15 @@ public:
 			for (int i = front_ + 1; i < capacity_; i++)
 				cout << setw(2) << queue_[i] << " ";
 
-			cout << endl << endl;
+			cout << endl
+				 << endl;
 		}
 		else // 비었을 경우
 		{
 			for (int i = 0; i < capacity_; i++)
 				cout << " - ";
-			cout << endl << endl;
+			cout << endl
+				 << endl;
 		}
 	}
 
@@ -181,10 +205,10 @@ public:
 		print_debug_ = flag;
 	}
 
-protected: // 뒤에서 상속해서 사용
-	T* queue_; // array for queue elements
+protected:			// 뒤에서 상속해서 사용
+	T *queue_;		// array for queue elements
 	int front_ = 0; // 시작 인덱스보다 하나 작은 값
-	int rear_ = 0; // 마지막 인덱스 (첫 값은 1에 추가)
-	int capacity_; // 빈 칸을 하나 둬야 하기 때문에 필요 메모리는 최대 저장량 + 1
+	int rear_ = 0;	// 마지막 인덱스 (첫 값은 1에 추가)
+	int capacity_;	// 빈 칸을 하나 둬야 하기 때문에 필요 메모리는 최대 저장량 + 1
 	bool print_debug_ = false;
 };
