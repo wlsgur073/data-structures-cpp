24c572cf9da13145dc6bd911becc1ce50a7bdc37
wlsgur073 <wlsgur073@gmail.com>
2025-01-19 19:43:14 +0900

    인접 리스트 커밋 완료. 드디어 끝났다..

---
+++ 1202_adjacentList/main.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+#include <cassert>
+
+#include "../shared/Stack.h"
+#include "../shared/Queue.h"
+
+using namespace std;
+
+template<typename T>
+class AdjListGraph // Adjacent List Graph
+{
+public:
+	struct Vertex
+	{
+		T item = T();
+	};
+
+	struct Node
+	{
+		//Vertex* vertex = nullptr;
+		int vertex = -1; // vertex의 index
+		Node* next = nullptr;
+	};
+
+	AdjListGraph(int max_vertices)
+	{
+		max_vertices_ = max_vertices; // capacity
+
+		vertices_ = new Vertex[max_vertices_];
+		list_ = new Node * [max_vertices_];
+		for (int i = 0; i < max_vertices_; i++)
+			list_[i] = nullptr;
+
+		n_ = 0;
+	}
+
+	~AdjListGraph()
+	{
+		delete[] vertices_;
+		delete[] list_;
+
+		if (visited_)
+			delete[] visited_;
+	}
+
+	void InsertVertex(T item)
+	{
+		vertices_[n_].item = item;
+		n_++;
+	}
+
+	void InsertEdge(int u, int v) // 여기서 u, v는 인덱스
+	{
+		assert(u < n_ && v < n_);
+
+		Node* temp = new Node{ v, list_[u] };
+		list_[u] = temp;
+	}
+
+	int Capacity()
+	{
+		return max_vertices_;
+	}
+
+	void Print()
+	{
+		for (int v = 0; v < n_; v++)
+		{
+			cout << vertices_[v].item << " : ";
+
+			Node* current = list_[v];
+			while (current)
+			{
+				cout << vertices_[current->vertex].item << " ";
+				current = current->next;
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+	}
+
+	void DepthFirstTraversal()
+	{
+		ResetVisited();
+
+		DepthFirstTraversal(0);
+
+		cout << endl;
+	}
+
+	void DepthFirstTraversal(int v) // v는 인덱스
+	{
+		cout << vertices_[v].item << " ";
+		visited_[v] = true;
+
+		Node* current = list_[v];
+		while (current)
+		{
+			int next = current->vertex;
+			if (!visited_[next])
+				DepthFirstTraversal(next);
+			current = current->next;
+		}
+	}
+
+	void IterDFT()
+	{
+		ResetVisited();
+
+		Stack<int> s;
+
+		s.Push(0); // 0번에서 시작
+
+		while (!s.IsEmpty())
+		{
+			int v = s.Top();
+			s.Pop();
+
+			if (!visited_[v])
+			{
+				cout << vertices_[v].item << " ";
+				visited_[v] = true;
+
+				Node* current = list_[v];
+				while (current)
+				{
+					int next = current->vertex;
+					if (!visited_[next])
+						s.Push(next);
+					current = current->next;
+				}
+				cout << "Stack: ";
+				s.Print();
+			}
+		}
+		cout << endl;
+	}
+
+	void BreadthFirstTraversal()
+	{
+		int v = 0; // 0번에서 시작
+
+		Queue<int> q;
+
+		ResetVisited();
+
+		visited_[v] = true;
+
+		q.Enqueue(v);
+
+		while (!q.IsEmpty())
+		{
+			v = q.Front();
+			q.Dequeue();
+
+			cout << vertices_[v].item << " ";
+
+			Node* current = list_[v];
+			while (current)
+			{
+				int next = current->vertex;
+				if (!visited_[next])
+				{
+					visited_[next] = true;
+					q.Enqueue(next);
+				}
+				current = current->next;
+			}
+
+			cout << "Queue: ";
+			q.Print();
+		}
+	}
+
+	void ResetVisited()
+	{
+		if (!visited_)
+			visited_ = new bool[max_vertices_];
+		for (int i = 0; i < max_vertices_; i++) visited_[i] = false; // 초기화
+	}
+
+private:
+	Vertex* vertices_ = nullptr;
+	Node** list_ = nullptr;
+	int n_ = 0;					// size
+	int max_vertices_ = 0;		// capacity
+
+	bool* visited_ = nullptr;
+};
+
+int main()
+{
+	AdjListGraph<int> g(7);
+
+	for (int i = 0; i < g.Capacity(); i++)
+		g.InsertVertex(i);
+
+	g.InsertEdge(0, 2);	g.InsertEdge(2, 0);
+	g.InsertEdge(0, 1);	g.InsertEdge(1, 0);
+	g.InsertEdge(1, 4);	g.InsertEdge(4, 1);
+	g.InsertEdge(1, 3);	g.InsertEdge(3, 1);
+	g.InsertEdge(2, 4);	g.InsertEdge(4, 2);
+	g.InsertEdge(3, 6);	g.InsertEdge(6, 3);
+	g.InsertEdge(4, 6);	g.InsertEdge(6, 4);
+	g.InsertEdge(5, 6);	g.InsertEdge(6, 5);
+
+	g.Print();
+
+	g.DepthFirstTraversal();
+	cout << endl;
+
+	g.IterDFT();
+
+	g.BreadthFirstTraversal();
+
+	return 0;
+}
